a2e9646f76b688cbca0eb55c8e1c8ae379692d37
TSVETKOV Viktor <viktor.tsvetkov@raiffeisen.ru>
2021-06-02 12:30:46 +0300

    Initial commit

---
+++ src/main/java/ru/raiffeisen/Application.java
@@ -0,0 +1,16 @@
+package ru.raiffeisen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
